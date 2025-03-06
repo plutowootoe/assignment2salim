f25f56c8bbe9306e25fbd298d30eff7267856537
caragh7 <morahancaragh@gmail.com>
2025-03-06 02:27:37 +0000

    - link fix

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Navbar } from "flowbite-react";
+import Link from "next/link";
 
 export default function Header() {
   return (
@@ -19,20 +20,20 @@ export default function Header() {
       {/* Navbar Menu */}
       <Navbar.Collapse className="font-medium flex flex-col md:flex-row md:space-x-8 rtl:space-x-reverse mt-4 md:mt-0 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
       <div className="flex flex-col md:flex-row md:space-x-8 text-md py-3 px-4 md:py-0">
-        <Navbar.Link href="#" active className="py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 dark:text-white md:dark:text-blue-500">
+        <Navbar.Link as={Link} href="#" active className="py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 dark:text-white md:dark:text-blue-500">
           Home (Book Now or List Events?)
         </Navbar.Link>
         {/* change these to next links  */}
-        <Navbar.Link href="/create" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500">
+        <Navbar.Link as={Link} href="/create" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500">
           Create
         </Navbar.Link>
-        <Navbar.Link href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500">
+        <Navbar.Link as={Link} href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500">
           Marketing
         </Navbar.Link>
-        <Navbar.Link href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500">
+        <Navbar.Link as={Link} href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500">
           Resale
         </Navbar.Link>
-        <Navbar.Link href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500">
+        <Navbar.Link as={Link} href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500">
           Admin?
         </Navbar.Link>
         </div>
